08a15e8d6abd624fe0a6f8fbad9ba2959df2f1a5
DonMega <LastDon@DonMega.local>
2019-05-29 23:31:40 -0700

    this is the update

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 console.log('hello world')
 console.log('hay dude')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+
+    let square = document.querySelector('#square')
+    let button = document.querySelector('#change-color')
+    //button.addEventListener('click', () +> )
+    square.style.backgroundColor = 'blue'
+}
+)
